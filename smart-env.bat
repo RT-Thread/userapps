45b1fc1334e03c3807ad838f7d33850a98aa0477
guozhanxin <guozhanxin@rt-thread.com>
2022-09-29 11:48:33 +0800

    fixed smart-env.bat on win10

--- smart-env.bat
+++ smart-env.bat
@@ -9,18 +9,18 @@
     @set RTT_CC=gcc
     @set RTT_EXEC_PATH=%cd%\tools\gnu_gcc\arm-linux-musleabi_for_i686-w64-mingw32\bin
     @set RTT_CC_PREFIX=arm-linux-musleabi-
-    @set PATH=%RTT_EXEC_PATH%;%PATH%
     @copy configs\def_config_arm .config
 ) else if %def_arch%==riscv64 (
     @set RTT_CC=gcc
     @set RTT_EXEC_PATH=%cd%\tools\gnu_gcc\riscv64-linux-musleabi_for_i686-w64-mingw32\bin
     @set RTT_CC_PREFIX=riscv64-unknown-linux-musl-
-    @set PATH=%RTT_EXEC_PATH%;%PATH%
     @copy configs\def_config_riscv64 .config
 ) else (
     @echo "ERROR:supported_arch=arm riscv64!"
     @goto EXIT
 )
+
+@set PATH=%RTT_EXEC_PATH%;%PATH%
 
 @echo "Arch      => %def_arch%"
 @echo "CC        => %RTT_CC%"
